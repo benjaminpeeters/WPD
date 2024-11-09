4440253c926ae7edfd841a22c742dfaf5222db58
benjaminpeeters <benjaminpeeters@protonmail.com>
2024-11-09 11:35:47 +0100

    Update .gitignore to track more files

--- dev/build_website.r
+++ dev/build_website.r
@@ -12,3 +12,7 @@ devtools::document(roclets = c("rd", "collate", "namespace"))
 # 3. If that succeeds, try building the site with more verbose output
 options(pkgdown.internet = FALSE) # Disable external internet access
 pkgdown::build_site(devel = TRUE, preview = FALSE)
+
+# Might require to install the latest version:
+# Install latest version from GitHub
+# remotes::install_github("r-lib/pkgdown")
